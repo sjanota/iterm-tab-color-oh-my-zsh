999b62079c14ef0d02625e59193b02e8739689ce
Szymon Janota <szymon.janota@pubnub.com>
2022-07-11 14:49:33 +0200

    set terminal background and work with kube

--- iterm-tab-color.plugin.zsh
+++ iterm-tab-color.plugin.zsh
@@ -1,36 +1,25 @@
 tcConfigFilePath="$(dirname "$0")/.tc-config"
-declare -A tcConfigColors
-declare -a orderedConfig
-while IFS="=" read -r configKey hexValue || [ -n "$hexValue" ]; do
-  if ! ( [[ $configKey == \#* ]]); then
-    orderedConfig+=( $configKey )
-    tcConfigColors[$configKey]+=$hexValue
-  fi
-done < $tcConfigFilePath
+$(dirname "$0")/get_current_bg_color.sh
+defaultBg=$(cat /tmp/color)
+rm /tmp/color
 
 function directory_tab_color() {
-  try_set_tab_color "$PWD"
-}
-
-function command_tab_color() {
-  try_set_tab_color "$1"
+  try_set_tab_color "kubecontext:$(kubectl config current-context)"
 }
 
 function try_set_tab_color() {
-  for k in $orderedConfig; do
-    if ( [[ "$1" =~ "$k" ]] ); then
-      iterm_tab_color "$tcConfigColors[$k]"
-      return 0
-    fi
-
+  if [[ -n $(grep ${1}= $tcConfigFilePath) ]]; then
+    color=$(grep ${1}= $tcConfigFilePath | awk -F= '{print $2}')
+    iterm_tab_color "$color"
+    return 0
+  else
     iterm_tab_color
-  done
+  fi
 }
 
 function iterm_tab_color() {
   if [ $# -eq 0 ]; then
-    # Reset tab color if called with no arguments
-    echo -ne "\033]6;1;bg;*;default\a"
+    echo -ne "\033]4;-2;${defaultBg}\a"
     return 0
   elif [ $# -eq 1 ]; then
     if ( [[ $1 == \#* ]] ); then
@@ -44,24 +33,10 @@ function iterm_tab_color() {
       GREEN_HEX=${1:2:2}
       BLUE_HEX=${1:4:2}
     fi
-
-    RED=$(( 16#${RED_HEX} ))
-    GREEN=$(( 16#${GREEN_HEX} ))
-    BLUE=$(( 16#${BLUE_HEX} ))
-
-    echo -ne "\033]6;1;bg;red;brightness;$RED\a"
-    echo -ne "\033]6;1;bg;green;brightness;$GREEN\a"
-    echo -ne "\033]6;1;bg;blue;brightness;$BLUE\a"
-
+    echo -ne "\033]4;-2;rgb:$RED_HEX/$GREEN_HEX/$BLUE_HEX\a"
     return 0
   fi
-
-  # If more than 1 argument, assume 3 arguments were passed
-  echo -ne "\033]6;1;bg;red;brightness;$1\a"
-  echo -ne "\033]6;1;bg;green;brightness;$2\a"
-  echo -ne "\033]6;1;bg;blue;brightness;$3\a"
 }
 
 alias tc='iterm_tab_color'
-preexec_functions=(${preexec_functions[@]} "command_tab_color")
 precmd_functions=(${precmd_functions[@]} "directory_tab_color")
